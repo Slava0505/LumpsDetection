60defcb228a0969fe152084e16b301c7af69ab3c
Slava0505 <31116179+Slava0505@users.noreply.github.com>
2022-04-15 03:03:36 +0700

    Added model loading

--- model.py
+++ model.py
@@ -11,8 +11,12 @@ class CustomUnetMpdel():
         3D Tensor of shape (x, y, num_channels)
     """
     def __init__(self, input_shape):
+        self.input_shape = input_shape
+
+
+    def initial_model(self):
         self.model = custom_unet(
-            input_shape,
+            self.input_shape,
             filters=32,
             use_batch_norm=True,
             dropout=0.3,
@@ -22,14 +26,14 @@ class CustomUnetMpdel():
         self.model.summary()
         self.model.compile(
             optimizer=Adam(), 
-            loss='binary_crossentropy',
-            metrics=[iou, iou_thresholded]
+            loss='binary_crossentropy'
         )
 
     def train(self, dataset_path = 'data/ratings_train.dat'):
         from utils import get_augmented_data_loader
         train_gen = get_augmented_data_loader('data/ratings_train.dat')
 
+        self.initial_model()
         model.fit_generator(
             train_gen,
             steps_per_epoch=200,
@@ -39,5 +43,6 @@ class CustomUnetMpdel():
     def evaluate(self, dataset_path = 'data/ratings_train.dat'):
         pass
 
-    def load_weights(self):
-        pass
+    def load_weights(self, model_path):
+        from tensorflow.keras.models import load_model
+        self.model = load_model(model_path)
